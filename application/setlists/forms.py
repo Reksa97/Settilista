93554727879c2be04b2cd7be2ad40f3cd2040b2e
Reko Kälkäjä <Reksa97@github.com>
2018-05-05 22:36:43 +0300

    fixed validators on forms

--- application/setlists/forms.py
+++ application/setlists/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, validators
 
 class SetlistForm(FlaskForm):
-    name = StringField("Setlist name", [validators.required()])
+    name = StringField("Setlist name", [validators.Length(min=1,max=144)])
 
     class Meta:
         csrf = False
--- application/songs/forms.py
+++ application/songs/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, validators
 
 class SongForm(FlaskForm):
-    name = StringField("Song name", [validators.required()])
-    artist = StringField("Artist name", [validators.required()])
+    name = StringField("Song name", [validators.Length(min=1,max=63)])
+    artist = StringField("Artist name", [validators.Length(min=1,max=63)])
     songkey = SelectField(u'Song key', choices=[('Am', 'Am'), ('A', 'A'), ('Bbm', 'Bbm'), ('Bb', 'Bb'), ('Bm', 'Bm'), ('B', 'B'), ('Cm', 'Cm'), ('C', 'C'),
     ('C#m', 'C#m'), ('C#', 'C#'), ('Dm', 'Dm'), ('D', 'D'), ('Ebm', 'Ebm'), ('Eb', 'Eb'), ('Em', 'Em'), ('E', 'E'), ('Fm', 'Fm'), ('F', 'F'), ('F#m', 'F#m'), ('F#', 'F#'),
     ('Gm', 'Gm'), ('G', 'G'), ('G#m', 'G#m'), ('Ab', 'Ab')])
@@ -17,18 +17,18 @@ class SetlistSongForm(FlaskForm):
     songkey = SelectField(u'Song key', choices=[('Am', 'Am'), ('A', 'A'), ('Bbm', 'Bbm'), ('Bb', 'Bb'), ('Bm', 'Bm'), ('B', 'B'), ('Cm', 'Cm'), ('C', 'C'),
     ('C#m', 'C#m'), ('C#', 'C#'), ('Dm', 'Dm'), ('D', 'D'), ('Ebm', 'Ebm'), ('Eb', 'Eb'), ('Em', 'Em'), ('E', 'E'), ('Fm', 'Fm'), ('F', 'F'), ('F#m', 'F#m'), ('F#', 'F#'),
     ('Gm', 'Gm'), ('G', 'G'), ('G#m', 'G#m'), ('Ab', 'Ab')])
-    notes = StringField("Notes")
+    notes = StringField("Notes", [validators.Length(max=255)])
     class Meta:
         csrf = False
 
 class EditSetlistSongForm(FlaskForm):
-    name = StringField("Song name", [validators.required()])
-    artist = StringField("Artist name", [validators.required()])
+    name = StringField("Song name", [validators.Length(min=1,max=63)])
+    artist = StringField("Artist name", [validators.Length(min=1,max=63)])
     length = IntegerField("Song length (seconds)", 
     [validators.NumberRange(min=0, message="length must be atleast 0")])
     songkey = SelectField(u'Song key', choices=[('Am', 'Am'), ('A', 'A'), ('Bbm', 'Bbm'), ('Bb', 'Bb'), ('Bm', 'Bm'), ('B', 'B'), ('Cm', 'Cm'), ('C', 'C'),
     ('C#m', 'C#m'), ('C#', 'C#'), ('Dm', 'Dm'), ('D', 'D'), ('Ebm', 'Ebm'), ('Eb', 'Eb'), ('Em', 'Em'), ('E', 'E'), ('Fm', 'Fm'), ('F', 'F'), ('F#m', 'F#m'), ('F#', 'F#'),
     ('Gm', 'Gm'), ('G', 'G'), ('G#m', 'G#m'), ('Ab', 'Ab')])
-    notes = StringField("Notes")
+    notes = StringField("Notes", [validators.Length(min=1,max=255)])
     class Meta:
         csrf = False
